95b3bbd71b35712945da374b14baa95d2cb6895e
Austin Kong <truekevin@gmail.com>
2024-03-20 14:13:30 -0400

    specified the min and max heap size of running java app

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -Xms256m -Xmx1024m -jar *.jar > /dev/null 2> /dev/null < /dev/null &
