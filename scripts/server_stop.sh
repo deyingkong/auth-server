d33c3e78a8a96a0f6c5c1ac4cf1c9946fb79aa71
Austin Kong <truekevin@gmail.com>
2024-03-20 16:01:15 -0400

    optimized stop script

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-for pid in $(ps -ef | awk '/java/ && /auth-server.jar/ {print $2}'); do sudo kill -9 $pid; done
+echo "Current directory: "
+echo $PWD
+
+for pid in $(ps -ef | pgrep java | awk '/java/ && /auth-server.jar/ {print $2}'); do sudo kill -9 $pid; done
